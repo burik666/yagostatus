2533567683f5fc57def5a2e5df7f50e923596da2
Andrey Burov <burik666@gmail.com>
2018-02-12 21:16:48 +0300

    Add blank widget (template)

---
+++ widgets/blank.go
@@ -0,0 +1,29 @@
+// Package widgets contains a YaGoStatus widgets.
+package widgets
+
+import (
+	"github.com/burik666/yagostatus/ygs"
+)
+
+// BlankWidget is a widgets template.
+type BlankWidget struct{}
+
+// Configure configures the widget.
+func (w *BlankWidget) Configure(cfg map[string]interface{}) error {
+	return nil
+}
+
+// Run starts the main loop.
+func (w *BlankWidget) Run(c chan []ygs.I3BarBlock) error {
+	return nil
+}
+
+// Event processes the widget events.
+func (w *BlankWidget) Event(event ygs.I3BarClickEvent) {}
+
+// Stop shutdowns the widget.
+func (w *BlankWidget) Stop() {}
+
+func init() {
+	ygs.RegisterWidget(BlankWidget{})
+}
